56b26b4f7e93c6197cd08048cd78f3f7d324dcd8
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2020-09-18 04:09:41 -0400

    CVE-2019-16303 - JHipster Vulnerability Fix - Use CSPRNG in RandomUtil
    This fixes a security vulnerability in this project where the `RandomUtil.java`
    file(s) were using an insecure Pseudo Random Number Generator (PRNG) instead of
    a Cryptographically Secure Pseudo Random Number Generator (CSPRNG) for
    security sensitive data.
    
    Signed-off-by: Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>

--- src/main/java/cn/demo/random/util/RandomUtil.java
+++ src/main/java/cn/demo/random/util/RandomUtil.java
@@ -2,35 +2,46 @@ package cn.demo.random.util;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
+import java.security.SecureRandom;
+
 /**
  * Utility class for generating random Strings. 
  */
 public class RandomUtil {
+	private static final SecureRandom SECURE_RANDOM = new SecureRandom();
 
 	private static final int DEF_COUNT = 20;
+
+    static {
+        SECURE_RANDOM.nextBytes(new byte[64]);
+    }
 	
 	private RandomUtil() {
 	}
+
+    private static String generateRandomAlphanumericString() {
+        return RandomStringUtils.random(DEF_COUNT, 0, 0, true, true, null, SECURE_RANDOM);
+    }
 	/**
-	 * 生成一个密码
+	 * ??????
 	 * @return
 	 */
 	public static String generatePassword() {
-		return RandomStringUtils.randomAlphanumeric(DEF_COUNT);
+		return generateRandomAlphanumericString();
 	}
 	
 	/**
-	 *  生成一个激活密码
+	 *  ????????
 	 * @return
 	 */
 	public static String generateActivationKey() {
-		return RandomStringUtils.randomNumeric(DEF_COUNT);
+		return generateRandomAlphanumericString();
 	}
 	/**
-	 *  生成一个重置密码
+	 *  ????????
 	 * @return
 	 */
 	public static String generateResetKey() {
-		return RandomStringUtils.randomNumeric(DEF_COUNT);
+		return generateRandomAlphanumericString();
 	}
 }
